49fa8f128818c5d38dddc99fe7d310b932e7178c
Jordan Thomas <jthomas135@hotmail.com>
2022-06-02 11:04:05 -0700

    commit for 12.1

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE candidates (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    industryConnected BOOLEAN NOT NULL
+);
